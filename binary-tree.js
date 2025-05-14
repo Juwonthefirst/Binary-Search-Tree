1f0f05e22f5e1d16184ecbe5e2a7b298943bedaa
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-05-14 09:13:05 +0100

    update

---
+++ binary-tree.js
@@ -0,0 +1,47 @@
+class Node {
+	constructor(value) {
+		this.value = value
+		this.leftNode = null
+		this.rightNode = null
+	}
+}
+
+const cleanUpArray = function(array) {
+	return [...new Set(array)].sort((a, b) => a - b)
+}
+
+const buildTree = function(array, start, end) {
+	
+	if (start > end) {
+		return null;
+	}
+	
+	const mid = start + Math.floor((end - start) / 2)
+	const node = new Node(array[mid])
+	node.leftNode = buildTree(array, start, mid - 1)
+	node.rightNode = buildTree(array, mid + 1, end)
+	return node
+}
+
+
+export class Tree {
+	constructor(array) {
+		this.array = cleanUpArray(array)
+		this.root = buildTree(this.array, 0, this.array.length - 1)
+	}
+	
+	insert(value, node = this.root) {
+		if (this.array.includes(value)) {
+			throw new Error('Value already exists in the tree')
+		}
+		
+		if (value < currentNode.value) {
+			currentNode = currentNode.leftNode
+		}
+		else if (value > currentNode.value) {
+			console.log(currentNode.rightNode)
+			currentNode = currentNode.rightNode
+		}
+		return currentNode
+	}
+}
---
+++ main.js
@@ -0,0 +1,4 @@
+import { Tree } from './binary-tree.js';
+
+const bsTree = new Tree([1,4,6,7,3,2,5,7, 8])
+console.log(bsTree.insert(9))
